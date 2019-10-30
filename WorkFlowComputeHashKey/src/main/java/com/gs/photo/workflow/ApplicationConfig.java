055ccf8fee38a274e39862fb7f0df3a2711c5afb
user <hdeslandes@granadasolutions.com>
2019-10-30 07:06:32 +0100

    Update

--- WorkFlowComputeHashKey/src/main/java/com/gs/photo/workflow/ApplicationConfig.java
+++ WorkFlowComputeHashKey/src/main/java/com/gs/photo/workflow/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package com.gs.photo.workflow;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ImportResource("file:${user.home}/config/cluster-client.xml")
 @PropertySource("file:${user.home}/config/application.properties")
 public class ApplicationConfig extends AbstractApplicationConfig {
 
