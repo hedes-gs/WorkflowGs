8ed32fa63960d9fb5464b0c38388b6f6987fd0b7
user <hdeslandes@granadasolutions.com>
2019-11-02 10:40:22 +0100

    Update

--- WorkFlowStorms/src/main/java/com/gsphotos/storms/translator/KeyValueTranslator.java
+++ WorkFlowStorms/src/main/java/com/gsphotos/storms/translator/KeyValueTranslator.java
@@ -8,8 +8,6 @@ import org.apache.storm.kafka.spout.RecordTranslator;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 
-import com.google.common.collect.ImmutableMap;
-
 public class KeyValueTranslator implements RecordTranslator<String, Bytes> {
 
 	/**
@@ -21,10 +19,7 @@ public class KeyValueTranslator implements RecordTranslator<String, Bytes> {
 	public List<Object> apply(ConsumerRecord<String, Bytes> record) {
 		String keyString = record.key();
 		Bytes valueAsByte = record.value();
-		return new Values(
-			ImmutableMap.of(
-				keyString,
-				valueAsByte.get()));
+		return new Values(keyString, valueAsByte.get());
 	}
 
 	@Override
