b75e595e52f28ef758db4e3b2454dbe49d1a4be5
hery <hdeslandes@granadasolutions.com>
2020-04-11 18:29:08 +0200

    Rename HbaseExifOrImageSerializer to HbaseExifOrImageOrWfEventsSerializer.java

---
+++ serializers/src/main/java/com/gs/photos/serializers/HbaseExifOrImageOrWfEventsSerializer.java
@@ -0,0 +1,27 @@
+package com.gs.photos.serializers;
+
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.Serializer;
+
+import com.workflow.model.ExchangedTiffData;
+import com.workflow.model.events.WfEvents;
+
+public class HbaseExifOrImageSerializer implements Serializer<Object> {
+
+    protected ExchangedDataSerializer exchangedDataSerializer = new ExchangedDataSerializer();
+    protected ByteArraySerializer     byteArraySerializer     = new ByteArraySerializer();
+    protected WfEventsSerializer      wfEventsSerializer      = new WfEventsSerializer();
+
+    @Override
+    public byte[] serialize(String topic, Object data) {
+        if (data instanceof byte[]) {
+            return this.byteArraySerializer.serialize(topic, (byte[]) data);
+        } else if (data instanceof ExchangedTiffData) {
+            return this.exchangedDataSerializer.serialize(topic, (ExchangedTiffData) data);
+        } else if (data instanceof WfEvents) { return this.wfEventsSerializer.serialize(topic, (WfEvents) data); }
+        throw new IllegalArgumentException("Unexpected object " + data);
+    }
+
+    public HbaseExifOrImageSerializer() { super(); }
+
+}
