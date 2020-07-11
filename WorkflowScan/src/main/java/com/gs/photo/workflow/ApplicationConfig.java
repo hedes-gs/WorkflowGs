6a94aa575865e1e774f118e04f024765dc209abf
hery <hdeslandes@granadasolutions.com>
2020-07-11 18:43:21 +0200

    backup

--- WorkflowScan/src/main/java/com/gs/photo/workflow/ApplicationConfig.java
+++ WorkflowScan/src/main/java/com/gs/photo/workflow/ApplicationConfig.java
@@ -1,8 +1,13 @@
 package com.gs.photo.workflow;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Properties;
 
 import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -13,6 +18,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import com.gs.photos.serializers.ComponentEventSerializer;
+import com.gs.photos.serializers.ImportEventDeserializer;
+import com.workflow.model.events.ComponentEvent;
+import com.workflow.model.events.ComponentEvent.ComponentType;
+import com.workflow.model.events.ImportEvent;
 import com.workflow.model.files.FileToProcess;
 
 @Configuration
@@ -35,6 +45,55 @@ public class ApplicationConfig extends AbstractApplicationConfig {
         settings.put("sasl.kerberos.service.name", "kafka");
         AbstractApplicationConfig.LOGGER.info("creating producer string string with config {} ", settings.toString());
         Producer<String, FileToProcess> producer = new KafkaProducer<>(settings);
+        return producer;
+    }
+
+    @Bean
+    public String createScanName() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            String hostname = ip.getHostName();
+            String scanName = hostname + "-" + ComponentType.SCAN;
+            return scanName;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "unit-test", havingValue = "false")
+    public Producer<String, ComponentEvent> producerToPublishSomeComponentEvent(
+        @Value("${bootstrap.servers}") String bootstrapServers
+    ) {
+        Properties settings = new Properties();
+        settings.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        settings.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, AbstractApplicationConfig.KAFKA_STRING_SERIALIZER);
+        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ComponentEventSerializer.class.getName());
+        settings.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name);
+        settings.put("sasl.kerberos.service.name", "kafka");
+        AbstractApplicationConfig.LOGGER.info("creating producer string string with config {} ", settings.toString());
+        Producer<String, ComponentEvent> producer = new KafkaProducer<>(settings);
+        return producer;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "unit-test", havingValue = "false")
+    public Consumer<String, ImportEvent> consumerForImportEvent(
+        @Value("${group.id}") String groupId,
+        @Value("${bootstrap.servers}") String bootstrapServers
+    ) {
+        Properties settings = new Properties();
+        settings.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, AbstractApplicationConfig.KAFKA_STRING_DESERIALIZER);
+        settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ImportEventDeserializer.class.getName());
+        settings.put(ConsumerConfig.CLIENT_ID_CONFIG, this.applicationId);
+        settings.put(ConsumerConfig.GROUP_ID_CONFIG, groupId + "-" + this.createScanName());
+        settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        settings.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5);
+        settings.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name);
+        settings.put("sasl.kerberos.service.name", "kafka");
+        Consumer<String, ImportEvent> producer = new KafkaConsumer<>(settings);
         return producer;
     }
 
