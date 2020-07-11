4226b9a7d4369cecae5e0f7ca75dac1977f2b8a8
hery <hdeslandes@granadasolutions.com>
2020-07-11 18:48:50 +0200

    Update

--- WorkFlow/ansible/tools/kafka/run.sh
+++ WorkFlow/ansible/tools/kafka/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd /opt/kafka/latest/bin
+export KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=65 -XX:+ExplicitGCInvokesConcurrent -Djava.awt.headless=true"
+export KAFKA_HEAP_OPTS="-Xmx1500m -Xms768m"
+
 export KAFKA_OPTS=" -Djava.security.auth.login.config=/opt/kafka/latest/config/client_jaas.conf -javaagent:/opt/kafka/latest/monitor/jmx_prometheus_javaagent-0.12.0.jar=7000:/opt/kafka/latest/monitor/config_prometheus.yml"
 ./kafka-server-start.sh /opt/kafka/latest/config/server.properties
