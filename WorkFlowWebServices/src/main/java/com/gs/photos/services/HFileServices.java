792f511870e411434de36025279cbe7cb7fe89a3
user <hdeslandes@granadasolutions.com>
2020-07-13 08:59:40 +0200

    update

---
+++ WorkFlowWebServices/src/main/java/com/gs/photos/services/HFileServices.java
@@ -0,0 +1,14 @@
+package com.gs.photos.services;
+
+import org.springframework.stereotype.Service;
+
+import com.workflow.model.dtos.ImageDto;
+
+@Service
+public class HFileServices implements IHFileServices {
+
+    @Override
+    public void delete(ImageDto imageToDelete) { // TODO Auto-generated method stub
+    }
+
+}
